1d7080a4080facc5c1285e227fc35a410ac9530e
damgor <damian.gorka94@gmail.com>
2020-11-11 19:06:28 +0100

    version 1.04 -> 1.05

--- src/global.ts
+++ src/global.ts
@@ -2,5 +2,5 @@
 
 // export const BACKEND_URL='http://localhost:8080';
 export const BACKEND_URL='https://heroku-list-app.herokuapp.com';
-export const VERSION: string="1.04";
+export const VERSION: string="1.05";
 export const APP_TITLE: string="ListApp";
